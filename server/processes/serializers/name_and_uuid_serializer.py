6cb7e2de4f89ab0307904e08659de27b332a7418
jtsay <jtsay@cloudreactor.io>
2022-05-31 01:17:10 -0700

    Documentation fix

--- server/processes/serializers/name_and_uuid_serializer.py
+++ server/processes/serializers/name_and_uuid_serializer.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class NameAndUuidSerializer(serializers.Serializer):
     """
     Identifies an entity in three ways: 1. UUID; 2. Name; and 3. URL.
-    When used to indentify an entity in a request method body, only one of
+    When used to identify an entity in a request method body, only one of
     uuid and name needs to be specified. If both are present, they must
     refer to the same entity or else the response will be a 400 error.
     """
