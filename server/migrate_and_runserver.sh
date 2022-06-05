9a9fee884f78d3675cde4c8c9f5c7d5ab37ba6b6
jtsay <jtsay@cloudreactor.io>
2022-06-04 20:08:48 -0700

    Use recommended settings for gunicorn

--- server/migrate_and_runserver.sh
+++ server/migrate_and_runserver.sh
@@ -4,4 +4,4 @@ set -Eeuxo pipefail
 
 python manage.py migrate
 python manage.py load_dynamic_fixtures
-exec gunicorn task_manager.wsgi --threads=3 --bind 0.0.0.0:8000
+exec gunicorn task_manager.wsgi --bind 0.0.0.0:8000 --workers=2 --threads=4 --worker-class=gthread --worker-tmp-dir /dev/shm
