46b18016943cc44877257bc8533529e500756deb
jtsay <jeff@cloudreactor.io>
2023-04-02 22:18:32 -0700

    Don't open client side checker UI overlay by default

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
       typescript: true,
       eslint: {
         lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+      },
+      overlay: {
+        initialIsOpen: false
       }
     }),
   ],
