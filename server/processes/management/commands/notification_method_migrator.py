31449ab78ba2c55683cf922d6cc5f3e7b56b8a1b
jtsay <jeff@cloudreactor.io>
2025-02-02 15:12:15 -0800

    Handle existing Notification Methods with the same name when migrating Alert Methods

--- server/processes/management/commands/notification_method_migrator.py
+++ server/processes/management/commands/notification_method_migrator.py
@@ -27,6 +27,13 @@ class Command(BaseCommand):
         run_env_count = 0
         with StatusUpdater(incremental_count_mode=True) as status_updater:
             for am in AlertMethod.objects.all():
+                np = NotificationProfile.objects.filter(name=str(am.name),
+                      created_by_group=am.created_by_group).first()
+
+                if np:
+                    logger.info(f"Found notification profile {np.uuid} / {np.name} for alert method {am.uuid}")
+                    continue
+
                 np = NotificationProfile.objects.filter(uuid=str(am.uuid)).first()
 
                 if np is None:
@@ -53,29 +60,36 @@ class Command(BaseCommand):
 
                 am_pagerduty_profile = am.pagerduty_profile
                 if am_pagerduty_profile:
-                    pd_ndm = PagerDutyNotificationDeliveryMethod.objects.filter(uuid=str(am_pagerduty_profile.uuid)).first()
+                    pd_ndm = PagerDutyNotificationDeliveryMethod.objects.filter(
+                        name=am_pagerduty_profile.name,
+                        created_by_group=am_pagerduty_profile.created_by_group).first()
 
-                    if pd_ndm is None:
-                        pd_ndm = PagerDutyNotificationDeliveryMethod(
-                            uuid=str(am_pagerduty_profile.uuid),
-                            name=am_pagerduty_profile.name,
-                            description=am_pagerduty_profile.description,
-                            run_environment=am_pagerduty_profile.run_environment,
-                            pagerduty_api_key=am_pagerduty_profile.integration_key,
-                            pagerduty_event_class_template=am.pagerduty_event_class_template or am_pagerduty_profile.default_event_class_template,
-                            pagerduty_event_component_template=am_pagerduty_profile.default_event_component_template,
-                            pagerduty_event_group_template=am.pagerduty_event_group_template or am_pagerduty_profile.default_event_group_template,
-                            created_by_user=am_pagerduty_profile.created_by_user,
-                            created_by_group=am_pagerduty_profile.created_by_group,
-                            created_at=am_pagerduty_profile.created_at,
-                            updated_at=am_pagerduty_profile.updated_at,
-                        )
+                    if pd_ndm:
+                        logger.info(f"Found PagerDuty notification delivery method {pd_ndm.uuid} / {pd_ndm.name} for PDP {am_pagerduty_profile.uuid}")
+                    else:
+                        pd_ndm = PagerDutyNotificationDeliveryMethod.objects.filter(uuid=str(am_pagerduty_profile.uuid)).first()
 
-                        logger.info(f"Creating PagerDuty notification profile {pd_ndm.uuid} / {pd_ndm.name} ...")
+                        if pd_ndm is None:
+                            pd_ndm = PagerDutyNotificationDeliveryMethod(
+                                uuid=str(am_pagerduty_profile.uuid),
+                                name=am_pagerduty_profile.name,
+                                description=am_pagerduty_profile.description,
+                                run_environment=am_pagerduty_profile.run_environment,
+                                pagerduty_api_key=am_pagerduty_profile.integration_key,
+                                pagerduty_event_class_template=am.pagerduty_event_class_template or am_pagerduty_profile.default_event_class_template,
+                                pagerduty_event_component_template=am_pagerduty_profile.default_event_component_template,
+                                pagerduty_event_group_template=am.pagerduty_event_group_template or am_pagerduty_profile.default_event_group_template,
+                                created_by_user=am_pagerduty_profile.created_by_user,
+                                created_by_group=am_pagerduty_profile.created_by_group,
+                                created_at=am_pagerduty_profile.created_at,
+                                updated_at=am_pagerduty_profile.updated_at,
+                            )
 
-                        pd_ndm.save()
+                            logger.info(f"Creating PagerDuty notification profile {pd_ndm.uuid} / {pd_ndm.name} ...")
 
-                        logger.info(f"Finished creating PagerDuty notification profile {pd_ndm.uuid} / {pd_ndm.name}")
+                            pd_ndm.save()
+
+                            logger.info(f"Finished creating PagerDuty notification profile {pd_ndm.uuid} / {pd_ndm.name}")
 
                     np.notification_delivery_methods.add(pd_ndm)
                     any_dm_added = True
@@ -83,28 +97,34 @@ class Command(BaseCommand):
 
                 am_email_profile = am.email_notification_profile
                 if am_email_profile:
-                    email_ndm = EmailNotificationDeliveryMethod.objects.filter(uuid=str(am_email_profile.uuid)).first()
+                    email_ndm = EmailNotificationDeliveryMethod.objects.filter(
+                        name=am_email_profile.name,
+                        created_by_group=am_email_profile.created_by_group).first()
 
-                    if email_ndm is None:
-                        logger.info(f"Creating email delivery method {np.uuid} / {np.name}")
-                        email_ndm = EmailNotificationDeliveryMethod(
-                            uuid=str(am_email_profile.uuid),
-                            name=am_email_profile.name,
-                            description=am_email_profile.description,
-                            run_environment=am_email_profile.run_environment,
-                            to_addresses=am_email_profile.to_addresses,
-                            cc_addresses=am_email_profile.cc_addresses,
-                            bcc_addresses=am_email_profile.bcc_addresses,
-                            created_by_user=am_pagerduty_profile.created_by_user,
-                            created_by_group=am_pagerduty_profile.created_by_group,
-                            created_at=am_pagerduty_profile.created_at,
-                            updated_at=am_pagerduty_profile.updated_at,
-                        )
-                        email_ndm.save()
+                    if email_ndm:
+                        logger.info(f"Found email notification delivery method {email_ndm.uuid} / {email_ndm.name} for EP {am_email_profile.uuid}")
+                    else:
+                        email_ndm = EmailNotificationDeliveryMethod.objects.filter(uuid=str(am_email_profile.uuid)).first()
+
+                        if email_ndm is None:
+                            logger.info(f"Creating email delivery method {am_email_profile.uuid} / {am_email_profile.name}")
+                            email_ndm = EmailNotificationDeliveryMethod(
+                                uuid=str(am_email_profile.uuid),
+                                name=am_email_profile.name,
+                                description=am_email_profile.description,
+                                run_environment=am_email_profile.run_environment,
+                                to_addresses=am_email_profile.to_addresses,
+                                cc_addresses=am_email_profile.cc_addresses,
+                                bcc_addresses=am_email_profile.bcc_addresses,
+                                created_by_user=am_pagerduty_profile.created_by_user,
+                                created_by_group=am_pagerduty_profile.created_by_group,
+                                created_at=am_pagerduty_profile.created_at,
+                                updated_at=am_pagerduty_profile.updated_at,
+                            )
+                            email_ndm.save()
 
                     np.notification_delivery_methods.add(email_ndm)
                     any_dm_added = True
-
 
                 if any_dm_added:
                     np.save()
