b45f0eb4bbed1bb6eb0948e898fc286c2c48844d
Jeff Tsay <jeff@cloudreactor.io>
2026-03-17 00:45:06 -0700

    Add missing import

--- server/task_manager/settings.py
+++ server/task_manager/settings.py
@@ -15,6 +15,8 @@ from datetime import timedelta
 
 import environ
 
+from csp.constants import SELF
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
