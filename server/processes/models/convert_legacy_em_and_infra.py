26ca431ac8728d746fb583f3d169c29fb375d93f
Jeff Tsay <jeff@cloudreactor.io>
2026-04-15 04:15:31 -0700

    Stop using AWS columns in Task Execution table

--- server/processes/models/convert_legacy_em_and_infra.py
+++ server/processes/models/convert_legacy_em_and_infra.py
@@ -2,17 +2,12 @@ import logging
 
 from typing import Any
 
-from ..common.utils import deepmerge_with_lists
 from ..execution_methods import (
-    INFRASTRUCTURE_TYPE_AWS,
     AwsSettings,
-    AwsEcsExecutionMethod,
     AwsEcsExecutionMethodSettings
 )
 from .run_environment import RunEnvironment
 from .task import Task
-from .task_execution import TaskExecution
-from .aws_ecs_service_load_balancer_details import AwsEcsServiceLoadBalancerDetails
 
 
 logger = logging.getLogger(__name__)
@@ -36,21 +31,6 @@ def compute_region(
 
     logger.warning(f"Can't determine AWS region from cluster ARN '{cluster_arn}'")
     return region
-
-
-def compute_region_from_task_execution(
-    task_execution: TaskExecution
-) -> str | None:
-    cluster_arn = task_execution.aws_ecs_cluster_arn
-    if not cluster_arn:
-        return None
-
-    if cluster_arn.startswith("arn:aws:ecs:"):
-        parts = cluster_arn.split(":")
-        return parts[3]
-
-    logger.warning(f"Can't determine AWS region from cluster ARN '{cluster_arn}'")
-    return None
 
 
 def extract_infra_from_run_environment(run_environment: RunEnvironment) -> dict[str, Any]:
@@ -117,84 +97,6 @@ def populate_run_environment_aws_ecs_configuration(run_environment: RunEnvironme
             aws_ecs_config = aws_ecs_settings.model_dump()
 
         run_environment.default_aws_ecs_configuration = aws_ecs_config
-
-        return True
-    else:
-        return False
-
-
-def extract_em(task_execution: TaskExecution) -> dict[str, Any]:
-    te = task_execution
-    return convert_empty_to_none_values({
-        'launch_type': te.aws_ecs_launch_type,
-        'cluster_arn': te.aws_ecs_cluster_arn,
-        'execution_role_arn': te.aws_ecs_execution_role,
-        'task_role_arn': te.aws_ecs_task_role,
-        'platform_version': te.aws_ecs_platform_version,
-        'task_definition_arn': te.aws_ecs_task_definition_arn,
-        'task_arn': te.aws_ecs_task_arn
-    })
-
-
-def extract_infra_from_task_execution(task_execution: TaskExecution) -> dict[str, Any]:
-    te = task_execution
-    task = te.task
-
-    task_region = compute_region(task)
-
-    log_stream_prefix: str | None = None
-
-    log_query = task.log_query
-    if log_query:
-        last_slash_index = log_query.rfind('/')
-        if last_slash_index >= 0:
-            log_stream_prefix = log_query[(last_slash_index + 1):]
-
-    aws_defaults = {
-        'region': task_region,
-        'network': convert_empty_to_none_values({
-            'region': task_region,
-            'subnets': task.aws_default_subnets,
-            'security_groups': task.aws_ecs_default_security_groups,
-            'assign_public_ip': task.aws_ecs_default_assign_public_ip
-        }),
-        'logging': {
-            'driver': 'awslogs',
-            'options': {
-                'region': task_region,
-                'group': log_query,
-                'create_group': 'true',
-                'stream_prefix': log_stream_prefix
-            }
-        }
-    }
-
-    aws = {
-        'network': convert_empty_to_none_values({
-            'subnets': te.aws_subnets,
-            'security_groups': te.aws_ecs_security_groups,
-            'assign_public_ip': te.aws_ecs_assign_public_ip
-        }),
-        'tags': te.aws_tags
-    }
-
-    return deepmerge_with_lists(aws_defaults, te.infrastructure_settings or {}, aws)
-
-
-def populate_task_execution_em_and_infra(task_execution: TaskExecution,
-        should_reset: bool=False) -> bool:
-    te = task_execution
-    if te.aws_ecs_task_definition_arn:
-        te.execution_method_type = AwsEcsExecutionMethod.NAME
-
-        if should_reset or (te.execution_method_details is None):
-            te.execution_method_details = extract_em(te)
-
-        if not te.infrastructure_type:
-            te.infrastructure_type = INFRASTRUCTURE_TYPE_AWS
-
-        if should_reset or (not te.infrastructure_settings):
-            te.infrastructure_settings = extract_infra_from_task_execution(te)
 
         return True
     else:
--- server/tests/factories/task_execution_factory.py
+++ server/tests/factories/task_execution_factory.py
@@ -1,12 +1,10 @@
 from processes.models import Execution, TaskExecution
-from processes.models.convert_legacy_em_and_infra import populate_task_execution_em_and_infra
 
 import factory
 from faker import Factory as FakerFactory
 
 from .execution_factory import ExecutionFactory
 from .task_factory import TaskFactory
-from .user_factory import UserFactory
 
 faker = FakerFactory.create()
 
@@ -19,8 +17,3 @@ class TaskExecutionFactory(ExecutionFactory):
     status = Execution.Status.RUNNING.value
     run_reason = TaskExecution.RunReason.EXPLICIT_START.value
     stop_reason = None
-
-    @factory.post_generation
-    def sanitize_em(task_execution: TaskExecution, create: bool, extracted, **kwargs):
-        populate_task_execution_em_and_infra(task_execution)
-        task_execution.save()
